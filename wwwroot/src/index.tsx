e883f967479eba9f95cb691b1b2d8c8b816ba3e2
MarekMichura <marekti012@gmail.com>
2024-06-05 10:01:35 +0200

    Copy files into bin

--- wwwroot/src/index.tsx
+++ wwwroot/src/index.tsx
@@ -7,14 +7,16 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-function Tmp(props: any) {
-  console.log(props);
-  return <div>siema<Outlet /></div>
-}
-
 root.render(
   <React.StrictMode>
-
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<div>path/ <Outlet /></div>}>
+          <Route path='div' element={<div>div</div>} />
+          <Route path='dom' element={<div>dom</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
