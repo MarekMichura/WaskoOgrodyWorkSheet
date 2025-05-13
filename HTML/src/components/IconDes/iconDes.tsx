2666ffe086a266d0a0c75ba7f6aaaf5372a91a37
JamJestJam <marekti012@gmail.com>
2025-05-13 08:49:20 +0200

    scale icons

--- HTML/src/components/IconDes/iconDes.tsx
+++ HTML/src/components/IconDes/iconDes.tsx
@@ -1,19 +1,41 @@
+import {motion} from 'framer-motion'
+import {useCallback, useState} from 'react'
+
+import RippleLink from '../form/ripple/rippleLink'
+
 import {type IIconDescriptionProps} from './_type/IIconDescriptionProps'
 import s from './css.module.scss'
 
 function IconDescription({icon, description, title, nr}: IIconDescriptionProps) {
+  const [hovered, setHovered] = useState(false)
+
+  const hover = useCallback(() => {
+    console.log('123')
+    setHovered(true)
+  }, [])
+
+  const leave = useCallback(() => {
+    setHovered(false)
+  }, [])
+
   return (
-    <>
-      <div style={{gridArea: `icon${nr}`}} className={s.icon}>
+    <RippleLink href="/" onMouseEnter={hover} onMouseLeave={leave}>
+      <motion.div
+        style={{gridArea: `icon${nr}`}}
+        variants={{open: {scale: 1.3, transition: {duration: 0.5}}, close: {scale: 1}}}
+        className={s.icon}
+        animate={hovered ? 'open' : 'close'}
+        onHoverStart={hover}
+        onHoverEnd={leave}>
         {icon}
-      </div>
+      </motion.div>
       <h2 style={{gridArea: `title${nr}`}} className={s.iconTitle}>
         {title}
       </h2>
       <p style={{gridArea: `content${nr}`}} className={s.iconDes}>
         {description}
       </p>
-    </>
+    </RippleLink>
   )
 }
 
